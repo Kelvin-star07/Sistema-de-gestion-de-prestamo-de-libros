2ce8309093f78be9fa0832bb56b38658551cdfbd
kelvin <kelvinkodaddiaz@gmail.com>
2025-05-19 14:15:41 -0400

    El program controla la aplicacion

--- Program.cs
+++ Program.cs
@@ -4,22 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Libro libro = new Libro("Cien años de soledad","Garcia Marquez","192-2899-1022",true);
+           
+            PresentarMenu miPrograma = new PresentarMenu();
 
-            Usuario usuario1 = new Usuario("kelvin Diaz Ramirez",35);
-
-
-            Biblioteca biblioteca = new Biblioteca();
-            biblioteca.agregarLibro(libro);
-            biblioteca.agregarUsuario(usuario1);
-
-
-            usuario1.agregarLibroPrestado(libro);
-            usuario1.obteneLibroPrestado();
-
-            
-            
-
+            miPrograma.menu();
             Console.ReadKey();
 
 
