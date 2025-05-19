158e04c005cd2ff2e08743600e5acdf852b05b00
kelvin <kelvinkodaddiaz@gmail.com>
2025-05-19 09:21:15 -0400

    se agrego una clase libro

---
+++ Libro.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoGestionBiblioteca
+{
+    public class Libro
+    {
+        public string Titulo {  get; set; }
+        public string Autor {  get; set; }
+        public string ISBN { get; set; }
+        public bool Disponible;
+
+
+        public Libro() 
+        { 
+        
+           
+        }
+
+
+        public Libro(string Titulo, string Autor,string isbn, bool disponible)
+        {
+            this.Titulo = Titulo;
+            this.Autor = Autor;
+            this.ISBN = isbn;
+            this.Disponible = disponible;
+
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ProyectoGestionBiblioteca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Libro libro = new Libro("Cien años de soledad","Garcia Marquez","192-2899-1022",true);
+
+            Usuario usuario1 = new Usuario("kelvin Diaz Ramirez",35);
+
+
+            Biblioteca biblioteca = new Biblioteca();
+            biblioteca.agregarLibro(libro);
+            biblioteca.agregarUsuario(usuario1);
+
+
+            usuario1.agregarLibroPrestado(libro);
+            usuario1.obteneLibroPrestado();
+
+            
+            
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
